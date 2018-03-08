fd0c28fec30fc0d84bb5981c6604ae51b89f2289
qw <2234196606@qq.com>
2018-03-08 10:15:46 +0800

    Initial Commit

---
+++ lunbo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lunbo
+//
+//  Created by qw on 2018/3/8.
+//  Copyright © 2018年 lianxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
